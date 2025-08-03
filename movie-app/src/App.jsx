49fbe8fa25ddcf64fde67e218d1eee483d8afa2a
Kinshuk Shukla <87925952+Knowledgelocater@users.noreply.github.com>
2025-08-03 09:39:50 +0530

    Optimized search

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect , useState } from "react";
 import Search from "./component/Search";
 import Spinner from "./component/Spinner";
 import MovieCard from "./component/MovieCard";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -21,6 +22,9 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+
+useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
   const fetchMovies = async(query = '') => {
     setIsLoading(true);
@@ -51,8 +55,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies(searchTerm);
-  }, [searchTerm]);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
   return (
 
     <main>
